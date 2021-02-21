fe50ad1c1b1ab0d59aac0013970e4f6606cd3e7a
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2021-02-21 23:27:39 +0100

    Write first tests

--- test/cubit/counter_cubit_test.dart
+++ test/cubit/counter_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:counter_app/cubit/counter_cubit.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -17,5 +18,19 @@ void main() {
         () {
       expect(counterCubit.state, CounterState(counterValue: 0));
     });
+
+    blocTest(
+      'The cubit should emit a CounterState(counterValue:1, wasIncremented: true) when cubit.increment() is called',
+      build: () => counterCubit,
+      act: (cubit) => cubit.increment(),
+      expect: [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+
+    blocTest(
+      'The cubit should emit a CounterState(counterValue:-1, wasIncremented: false) when cubit.decrement() is called',
+      build: () => counterCubit,
+      act: (cubit) => cubit.decrement(),
+      expect: [CounterState(counterValue: -1, wasIncremented: false)],
+    );
   });
 }
