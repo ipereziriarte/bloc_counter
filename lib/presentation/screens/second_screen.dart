35e03246a246ab56dc75bff47a96b6f9aaf00f94
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2021-02-22 00:13:58 +0100

    Basic injection of bloc in navigation

---
+++ lib/presentation/screens/second_screen.dart
@@ -0,0 +1,104 @@
+import 'package:counter_app/domain/cubits/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SecondScreen extends StatefulWidget {
+  SecondScreen({Key key, this.title, this.color}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  final Color color;
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: BlocListener<CounterCubit, CounterState>(
+          listener: (context, state) {
+            if (state.wasIncremented) {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Incremented!'),
+                duration: Duration(milliseconds: 300),
+              ));
+            } else {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Decremented!'),
+                duration: Duration(milliseconds: 300),
+              ));
+            }
+          },
+          child: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                BlocBuilder<CounterCubit, CounterState>(
+                  builder: (context, state) {
+                    return Text(
+                      state.counterValue.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).decrement();
+                      },
+                      tooltip: 'Decrement',
+                      child: Icon(Icons.remove),
+                      heroTag: "decrement2",
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).increment();
+                      },
+                      tooltip: 'Increment',
+                      child: Icon(Icons.add),
+                      heroTag: "increment2",
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                MaterialButton(
+                  color: widget.color,
+                  onPressed: () {},
+                  child: Text('Go to Second Screen'),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
